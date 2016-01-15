5d53a281bccbce0b3b6e1ca659dada2c92b9fcac
tbauer516 <bauer.tyler7@yahoo.com>
2016-01-14 22:52:53 -0800

    testing php

--- facebook-php/proxy.php
+++ facebook-php/proxy.php
@@ -8,18 +8,22 @@ echo 'after reporting<br>';
 //App Info, needed for Auth
 $app_id = '647207678755391';
 $app_secret = '59be331e39208086fd66d5c30e5b63fe';
+// set page id
+$pageid = '147513818956534';
 
 $token = 'CAAJMocfJwj8BAIZCdERPi0vAD23BZCv8soKBZASh9BAZAUijTFsgdj0oOedaE9rPwjARhAaAaj03ZAIQwCEXffMjQCs0LxgQCvmXZA0p8Pa1o9wjZBLgj3TevIZCKzZCIgnITPSjo4s4hdnjk2tiQaTe95zxLJ8Yezg9i8ZCedNsR9aVuDmMGaroyZA';
 $fb->setDefaultAccessToken($token);
 
-// set page id
-$pageid = '147513818956534';
-echo 'before fb<br>';
 $fb = new Facebook\Facebook([
 	'app_id' => '647207678755391',
 	'app_secret' => '59be331e39208086fd66d5c30e5b63fe',
 	'default_graph_version' => 'v2.5',
 ]);
+
+echo 'before session<br>'
+
+FacebookSession::setDefaultApplication($app_id, $app_secret);
+$session = new FacebookSession($token);
 
 echo 'before try<br>';
 
