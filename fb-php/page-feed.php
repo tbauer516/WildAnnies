9c2ff750d82e4ba8cf5674874c165a579fac7f03
tbauer516 <bauer.tyler7@yahoo.com>
2016-01-15 14:45:46 -0800

    php

--- fb-php/page-feed.php
+++ fb-php/page-feed.php
@@ -9,14 +9,22 @@ header('Content-Type: application/json; charset=utf-8');
 
 $pageid = '147513818956534';
 $authToken = 'access_token=CAAJMocfJwj8BAIZCdERPi0vAD23BZCv8soKBZASh9BAZAUijTFsgdj0oOedaE9rPwjARhAaAaj03ZAIQwCEXffMjQCs0LxgQCvmXZA0p8Pa1o9wjZBLgj3TevIZCKzZCIgnITPSjo4s4hdnjk2tiQaTe95zxLJ8Yezg9i8ZCedNsR9aVuDmMGaroyZA';
-$field = 'general_info';
-$edge = 'posts';
+$fields = array(
+	0 => 'name',
+	1 => 'description',
+	2 => 'cover'
+);
+$edges = array(
+	0 => 'picture',
+	1 => 'posts'
+);
 $graphField = "https://graph.facebook.com/" . $pageid . "?fields=" . $field . "&" . $authToken;
 $graphEdge = "https://graph.facebook.com/" . $pageid . "/" . $edge . "?" . $authToken;
 
 //Echo back json to read client side.
 echo $graphField;
 echo '<br>';
+$field = $fields[0];
 echo fetchUrl("{$graphField}");
 echo '<br>';
 echo '<br>';
