98bae9ffd37ca0621a617a95507bdf2d5a861eca
tbauer516 <bauer.tyler7@yahoo.com>
2016-01-15 14:31:35 -0800

    php

--- fb-php/page-feed.php
+++ fb-php/page-feed.php
@@ -8,13 +8,11 @@ header('Content-Type: application/json; charset=utf-8');
 // $_JSON = json_decode(file_get_contents("php://input"), true);
 
 $pageid = '147513818956534';
+$authToken = 'access_token=CAAJMocfJwj8BAIZCdERPi0vAD23BZCv8soKBZASh9BAZAUijTFsgdj0oOedaE9rPwjARhAaAaj03ZAIQwCEXffMjQCs0LxgQCvmXZA0p8Pa1o9wjZBLgj3TevIZCKzZCIgnITPSjo4s4hdnjk2tiQaTe95zxLJ8Yezg9i8ZCedNsR9aVuDmMGaroyZA';
 $field = 'general_info';
 $edge = 'posts';
-$graphField = "https://graph.facebook.com/" . $pageid . "?fields=" . $field;
-$graphEdge = "https://graph.facebook.com/" . $pageid . "/" . $edge;
-
-//retrieve auth token
-$authToken = 'access_token=CAAJMocfJwj8BAIZCdERPi0vAD23BZCv8soKBZASh9BAZAUijTFsgdj0oOedaE9rPwjARhAaAaj03ZAIQwCEXffMjQCs0LxgQCvmXZA0p8Pa1o9wjZBLgj3TevIZCKzZCIgnITPSjo4s4hdnjk2tiQaTe95zxLJ8Yezg9i8ZCedNsR9aVuDmMGaroyZA';
+$graphField = "https://graph.facebook.com/" . $pageid . "?{$authToken}&fields=" . $field;
+$graphEdge = "https://graph.facebook.com/" . $pageid . "/" . $edge . "?" . $authToken;
 
 //Echo back json to read client side.
 echo fetchUrl("{$graphField}?{$authToken}");
